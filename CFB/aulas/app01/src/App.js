8afb9b464bece71aed530dc768b38a30f5f60be3
Alef A. Araujo <123469430+alefalves4867@users.noreply.github.com>
2023-05-25 20:38:23 -0300

    props aula

--- CFB/aulas/app01/src/App.js
+++ CFB/aulas/app01/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './App.css'
-import Hd1 from './componentes/Header1'
+import Hd1 from './componentes/Header1.js'
+import Dados from './componentes/Dados.js'
 
 
 const channel = 'Tzion Brazil'
 const App = ()=>{
   return(
     <>
-    <Hd1></Hd1>/*hd1 */
-
+    <Hd1/>
+    <Dados canal='Tzion Brazil'/>
     </>
   )
 }
--- CFB/aulas/app01/src/componentes/Header1.js
+++ CFB/aulas/app01/src/componentes/Header1.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Header1(){
     return(
         <header>
-            שלום
+             <section>
+            <p>שלום עליכם מלכי השלום מלכי אליון</p>
+            </section>
         </header>
 
     )
