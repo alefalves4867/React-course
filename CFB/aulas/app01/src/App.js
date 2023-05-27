34a4216a1229e7f7cdd837fcc677cfc054c2bf5a
Alef A. Araujo <123469430+alefalves4867@users.noreply.github.com>
2023-05-26 21:39:36 -0300

    aula 07

--- CFB/aulas/app01/src/App.js
+++ CFB/aulas/app01/src/App.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './App.css'
 import Hd1 from './componentes/Header1.js'
 import Dados from './componentes/Dados.js'
+import corpo from './componentes/Body.js'
 
 
-const channel = 'Tzion Brazil'
 const App = ()=>{
   return(
     <>
     <Hd1/>
-    <Dados canal='Tzion Brazil'/>
+    <Dados canal={corpo()}/>
     </>
   )
 }
--- CFB/aulas/app01/src/componentes/Dados.js
+++ CFB/aulas/app01/src/componentes/Dados.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default function Dados(props){
+
+    
     return(
         <p>Canal: {props.canal}</p>
        
