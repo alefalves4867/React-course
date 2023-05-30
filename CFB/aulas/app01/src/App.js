3808e9e617ec5573b7a03aa1685fc62a3f4dcf80
Alef A. Araujo <123469430+alefalves4867@users.noreply.github.com>
2023-05-30 20:09:27 -0300

    aula 11 funções no react

--- CFB/aulas/app01/src/App.js
+++ CFB/aulas/app01/src/App.js
@@ -6,9 +6,14 @@ import State from './componentes/State.js'
 const App = ()=>{
   const [num,setNum]=useState(0)
 
+  const cancelar = (obj) => {
+    return obj.preventDefault()
+  }
+
   return(
     <>
     <State setNum = {setNum} num = {num}/>
+    <a href='www.youtube.com' target='_blank' onClick={(e)=>cancelar(e)}>YT</a>
     </>
   )
 }
