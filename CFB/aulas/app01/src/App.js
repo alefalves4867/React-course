b0d1517a410d5662705e2cc57a7c9bf73fc06c6b
Alef A. Araujo <123469430+alefalves4867@users.noreply.github.com>
2023-06-01 20:52:17 -0300

    aula 13
    12 Sivan 5783

--- CFB/aulas/app01/src/App.js
+++ CFB/aulas/app01/src/App.js
@@ -1,21 +1,24 @@
-import React,{useState} from 'react'
+import React from 'react'
 import './App.css'
-import State from './componentes/State.js'
-import Login from './componentes/Login.js'
+
 
 
 const App = ()=>{
-  const [num,setNum]=useState(0)
-
-  const cancelar = (obj) => {
-    return obj.preventDefault()
-  }
+  const carros = ['SUV', 'Golf', 'Meseit', 'Mekhonit']
+  const lista = carros.map((e)=>{
+    return(
+      <>
+      <div>{e}</div>
+      <p>-----</p>
+      </>
+    )
+    
+  })
 
   return(
     <>
-    <State setNum = {setNum} num = {num}/>
-    <a href='www.youtube.com' target='_blank' onClick={(e)=>cancelar(e)}>YT</a>
-    <Login/>
+    <p>{lista}</p>
+    
     </>
   )
 }
