32641e9ab6d2ae40f14536576cfa509b98a3e822
Alef A. Araujo <123469430+alefalves4867@users.noreply.github.com>
2023-06-04 21:24:56 -0300

    aula 15
    utilizando um unico useState para mudar as propriedades de ais de u elemento

--- CFB/aulas/app01/src/componentes/aula14.js
+++ CFB/aulas/app01/src/componentes/aula14.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 export default function aula14(props){
-    const [name, setName] = props.useState('')
+    const [name, setName] = props.useState({'nome':'', 'dia':''})
 
     return(
         <>
         <h1>שלום עליכם</h1>
 
-        <input type='text' name='fnome' value={name} onChange={(e)=>setName(e.target.value)}/>
+        <input type='text' name='fnome' value={name.nome} onChange={(e)=>setName({'nome':e.target.value})}/>
 
-        <p>Nome digitado: {name}</p>
+        <p>Nome digitado: {name.nome}</p>
         </>
     )
 
